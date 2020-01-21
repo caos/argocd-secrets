9dc2064ca620a86213aaedaa0a3483c8a0959439
Christian <christian@caos.ch>
2020-01-21 12:24:31 +0100

    caos semantic release

--- .releaserc.js
+++ .releaserc.js
@@ -3,5 +3,9 @@ module.exports = {
     plugins: [ 
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator"
+        "@semantic-release/github",
+        ["@semantic-release/exec", {
+            "prepareCmd": "echo '::set-env name=CAOS_NEXT_VERSION::v${nextRelease.version}'"
+            }],
     ]
   };
