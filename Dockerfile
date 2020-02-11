bceb55d7310df5394a841229d90369b82c3460f5
Christian <christian@caos.ch>
2020-02-11 16:18:55 +0100

    minor: added jq and json parser

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ ARG GOPASS_VERSION="1.8.6"
 RUN apt-get update && \
     apt-get install -y \
         wget \
+        jq \
         gpg &&\
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* && \
--- scripts/initialize_gopass.sh
+++ scripts/initialize_gopass.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 #example: initialize_gopass.sh git@github.com:<YOURORGANIZATION>/<YOURSECRETSTORE> <YOURSECRETSTORE>
 
-SECRET_REPOSITORY=$1
-SECRET_STORE=$2
+REPOSITORY_LIST_JSON=$1
 LOGFILE=gopass.log
 
 GOPASS_VERSION="1.8.6"
-
 
 # Script for initial secret and key declaration for gpg/gopass
 set -e
@@ -32,13 +30,27 @@ function  initialize_gopass_store {
 gopass  --yes init --crypto gpg-id $(gpg --list-keys --with-colons  | awk -F: '/pub:/ {print $5}') --rcs gitcli &>> $LOGFILE
 }
 
+function unmarshall_json_and_clone_remote {
+# parse json and isolate stores
+i=0
+for store in $(echo ${REPOSITORY_LIST_JSON} | jq -r ".stores[].storename"); do
+  SECRET_STORE=$(echo $REPOSITORY_LIST_JSON | jq -r ".stores[$i].storename")
+  SECRET_REPOSITORY=$(echo $REPOSITORY_LIST_JSON | jq -r ".stores[$i].directory")
+  clone_remote_gopass_store
+  i=$i+1
+  #savety sleep for git checkout and gopass
+  sleep 5
+done
+}
+
+
 function clone_remote_gopass_store {
 # checkout the customers passtore
-gopass --yes clone $SECRET_REPOSITORY $SECRET_STORE --sync gitcli &>> $LOGFILE
+echo gopass --yes clone $SECRET_REPOSITORY $SECRET_STORE --sync gitcli # &>> $LOGFILE
 }
+
 
 initialize_ssh
 import_and_trust_gpg-key
 initialize_gopass_store
-clone_remote_gopass_store
-
+unmarshall_json_and_clone_remote
